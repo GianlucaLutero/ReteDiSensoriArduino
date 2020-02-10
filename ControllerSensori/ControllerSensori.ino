a5eed8d8a74eefc2e95329584ad3948a859ce6f7
GianlucaLutero <gianluca.lutero@studenti.unipr.it>
2020-02-11 00:52:55 +0100

    Primo prototipo
    - Codice sensore di parcheggio prima versione terminata
    - Codice controller prima versione terminata, manca solo il buzzer

--- ControllerSensori/ControllerSensori.ino
+++ ControllerSensori/ControllerSensori.ino
@@ -5,13 +5,113 @@
  *  - angolo di sterzata
  */
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include <LiquidCrystal_I2C.h>
+
+// Modulo nrf24l01
+#define CE 9
+#define SCN 10
+#define SCK 13
+#define MOSI 11
+#define MISO 12
+
+// Display I2C
+#define SDA A4
+#define SCL A5
+
+// LED RGB
+#define RED 7
+#define GREEN 6
+#define BLUE 5
+
+RF24 radio(CE,SCN);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const byte address[6] ="00001";
+
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(RED, redValue);
+  analogWrite(GREEN, greenValue);
+  analogWrite(BLUE, blueValue);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+
+  // Inizializzo il display lcd
+  lcd.init();
+  lcd.backlight();
+
+  // Inizializzo il modulo radio
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  // Inizializzo il led
+  pinMode(RED,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+  pinMode(BLUE,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if(radio.available()){
+    float data[3];
+
+    // Ricevo i dati
+    radio.read(&data,sizeof(data));
+
+    // Stampo i dati su display
+    lcd.clear();
+    lcd.setCursor(0,0);
+
+    if (data[0] <= 10 || data[1] <= 10 || data[2] <= 10){
+      lcd.print("Porco Dio");
+      lcd.setCursor(0,1);
+      lcd.print("sbatti !!!!");
+    }else{
+      lcd.print("L:");
+      lcd.print(data[0]);
+      lcd.print(" C:");
+      lcd.print(data[1]);
+      lcd.setCursor(0,1);
+      lcd.print("R:");
+      lcd.print(data[2]);
+    }
+
+    // Accendo il led in base alla distanza
+    float min = data[0];
+
+    for(int i = 1; i<= 2; ++i){
+      if(data[i] <= min){
+        min = data[i];
+      }
+    }
+
+    if(min >= 60){
+      setColor(255,0,255);
+    }
+    
+    if(min >= 10 && min < 60){
+      setColor(0,0,255);  
+    }
+
+    if(min < 10){
+      setColor(0,255,255);
+    }
+    
+    Serial.print("Dist 00: ");
+    Serial.println(data[0]);
+    Serial.print("Dist 01: ");
+    Serial.println(data[1]);
+    Serial.print("Dist 02: ");
+    Serial.println(data[2]);
+  }
 
 }
--- ModuloParcheggio/ModuloParcheggio.ino
+++ ModuloParcheggio/ModuloParcheggio.ino
@@ -1,12 +1,27 @@
 /*
  * Sensore di parcheggio e luminosita'
  */
-#define TRIG00 13
-#define ECHO00 12
-#define TRIG01 11
-#define ECHO01 10
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+#define TRIG00 8
+#define ECHO00 7
+#define TRIG01 6
+#define ECHO01 5
+#define TRIG02 4
+#define ECHO02 3
 
+// Modulo nrf24l01
+#define CE 9
+#define SCN 10
+#define SCK 13
+#define MOSI 11
+#define MISO 12
 
+RF24 radio(CE,SCN);
+
+const byte address[6] = "00001";
 
 float distance(int trig, int echo){
   int duration = 0;
@@ -25,12 +40,23 @@ float distance(int trig, int echo){
 }
 
 void setup() {
- 
+
+  // Left sensor
   pinMode(TRIG00,OUTPUT);
   pinMode(ECHO00,INPUT);
-  
+
+  // Center sensor
   pinMode(TRIG01,OUTPUT);
   pinMode(ECHO01,INPUT);
+
+  // Right sensor.h
+  pinMode(TRIG02,OUTPUT);
+  pinMode(ECHO02,INPUT);
+
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
   
   Serial.begin(9600);
   Serial.println("Distanze misurate dal sensore ad ultrasuoni:");
@@ -41,18 +67,31 @@ void loop() {
   // put your main code here, to run repeatedly:
   float dist00 = 9999;
   float dist01 = 9999;
+  float dist02 = 9999;
+  float data[3];
   
   Serial.print("Distanza sensore 00:");
   dist00 = distance(TRIG00,ECHO00);
   Serial.println(dist00);
   
   Serial.print("Distanza sensore 01:");
-  dist01 =distance(TRIG01,ECHO01);
+  dist01 = distance(TRIG01,ECHO01);
   Serial.println(dist01);
+
+  Serial.print("Distanza sensore 02:");
+  dist02 = distance(TRIG02,ECHO02);
+  Serial.println(dist02);
+
+  data[0]=dist00;
+  data[1]=dist01;
+  data[2]=dist02;  
+  
+  radio.write(&data,sizeof(data));
   delay(1000);
 
-  if (dist00 <= 10 || dist01 <= 10){
+  if (dist00 <= 10 || dist01 <= 10 || dist02 <= 10){
     Serial.println("Troppo vicino!!!");
+
   }
   
 }
